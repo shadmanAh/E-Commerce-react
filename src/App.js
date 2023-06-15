3861f04642c9e1d031acb9c5765037f30d4c3156
shadmanAh <shadman061@gmail.com>
2023-06-15 10:36:42 +0330

    first commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Navbar, Sidebar, Footer } from './components'
+import {
+  Home,
+  SingleProducts,
+  Cart,
+  Checkout,
+  Error,
+  About,
+  Products,
+  PrivateRoute,
+} from './pages'
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/cart">
+          <Cart />
+        </Route>
+        <Route exact path="/products">
+          <Products />
+        </Route>
+        <Route exact path="/products/:id" children={<SingleProducts />} />
+        <Route exact path="/checkout">
+          <Checkout />
+        </Route>
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
+  )
+}
+
+export default App
---
+++ src/reducers/products_reducer.js
@@ -0,0 +1,22 @@
+import {
+  SIDEBAR_OPEN,
+  SIDEBAR_CLOSE,
+  GET_PRODUCTS_BEGIN,
+  GET_PRODUCTS_SUCCESS,
+  GET_PRODUCTS_ERROR,
+  GET_SINGLE_PRODUCT_BEGIN,
+  GET_SINGLE_PRODUCT_SUCCESS,
+  GET_SINGLE_PRODUCT_ERROR,
+} from '../actions'
+
+const products_reducer = (state, action) => {
+  if (action.type === SIDEBAR_OPEN) {
+    return { ...state, isSidebarOpen: true }
+  }
+  if (action.type === SIDEBAR_CLOSE) {
+    return { ...state, isSidebarOpen: false }
+  }
+  throw new Error(`No Matching "${action.type}" - action type`)
+}
+
+export default products_reducer
