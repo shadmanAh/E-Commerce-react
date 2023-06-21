24811dc7b276dbec089b262b25c13927cf2ebe3b
shadmanAh <shadman061@gmail.com>
2023-06-21 17:58:33 +0330

    create Single product and details

--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -17,7 +17,7 @@ const products_reducer = (state, action) => {
     return { ...state, isSidebarOpen: false }
   }
   if (action.type === GET_PRODUCTS_BEGIN) {
-    return { ...state, products_loading: true }
+    return { ...state, products_loading: true, products_error: false }
   }
   if (action.type === GET_PRODUCTS_SUCCESS) {
     const featured_products = action.payload.filter(
@@ -28,10 +28,33 @@ const products_reducer = (state, action) => {
       products_loading: false,
       products: action.payload,
       featured_products,
+      products_error: false,
     }
   }
   if (action.type === GET_PRODUCTS_ERROR) {
     return { ...state, products_loading: false, products_error: true }
+  }
+  if (action.type === GET_SINGLE_PRODUCT_BEGIN) {
+    return {
+      ...state,
+      single_product_loading: true,
+      single_product_error: false,
+    }
+  }
+  if (action.type === GET_SINGLE_PRODUCT_SUCCESS) {
+    return {
+      ...state,
+      single_product_loading: false,
+      single_product_error: false,
+      single_product: action.payload,
+    }
+  }
+  if (action.type === GET_SINGLE_PRODUCT_ERROR) {
+    return {
+      ...state,
+      single_product_loading: false,
+      single_product_error: true,
+    }
   }
   throw new Error(`No Matching "${action.type}" - action type`)
 }
