40ceab244f618ec70c39bebcfb65b77b201e0d25
shadmanAh <shadman061@gmail.com>
2023-08-09 10:02:49 +0330

    add login, logout and authentication

--- src/index.js
+++ src/index.js
@@ -9,14 +9,26 @@ import { CartProvider } from './context/cart_context'
 import { UserProvider } from './context/user_context'
 import { Auth0Provider } from '@auth0/auth0-react'
 
+// dev-pt2knjea3ymclpjh.us.auth0.com
+// ip3Km9RcbB9BSog8HIricBAcPRZA6vGA
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>
+  <Auth0Provider
+    domain="dev-pt2knjea3ymclpjh.us.auth0.com"
+    clientId="ip3Km9RcbB9BSog8HIricBAcPRZA6vGA"
+    redirectUri={window.location.origin}
+    cacheLocation="localstorage"
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>
 )
