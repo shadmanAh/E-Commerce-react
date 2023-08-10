896cc1759c294094d36685ca8373f45aa447938f
shadmanAh <shadman061@gmail.com>
2023-08-10 08:54:10 +0330

    add AuthWrapper component

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -5,12 +5,12 @@ import { useAuth0 } from '@auth0/auth0-react'
 import { useUserContext } from '../context/user_context'
 
 const PrivateRoute = ({ children, ...rest }) => {
-  const { myUser } = useUserContext()
+  const { user } = useAuth0()
   return (
     <Route
       {...rest}
       render={() => {
-        return myUser ? children : <Redirect to="/"></Redirect>
+        return user ? children : <Redirect to="/"></Redirect>
       }}
     ></Route>
   )
