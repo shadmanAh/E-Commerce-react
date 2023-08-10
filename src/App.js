7e7a9ea39b777c481b82c589c1b1f7c5d7d01924
shadmanAh <shadman061@gmail.com>
2023-08-10 08:36:47 +0330

    private route

--- src/App.js
+++ src/App.js
@@ -31,9 +31,9 @@ function App() {
           <Products />
         </Route>
         <Route exact path="/products/:id" children={<SingleProducts />} />
-        <Route exact path="/checkout">
+        <PrivateRoute exact path="/checkout">
           <Checkout />
-        </Route>
+        </PrivateRoute>
         <Route path="*">
           <Error />
         </Route>
